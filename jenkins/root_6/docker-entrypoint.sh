c224d770e3da0f665690a2c998467a03635f5689
Kamil Rakoczy <grey277@wp.pl>
2018-07-07 11:28:19 +0200

    Add files for jenkins test

---
+++ jenkins/root_6/docker-entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+function executeCommand {
+    $@
+    rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+    echo "Exit code[" $@ "]: $rc"
+}
+
+executeCommand "mkdir -p build"
+executeCommand "cd build"
+executeCommand "export CMAKE_LIBRARY_PATH=$CMAKE_LIBRARY_PATH:/framework-dependencies/lib"
+executeCommand "export CMAKE_INCLUDE_PATH=$CMAKE_INCLUDE_PATH:/framework-dependencies/include"
+executeCommand "export CLHEP_DIR=/clhep-install"
+executeCommand "export CLHEP_INCLUDE_DIR=/clhep-install/include"
+executeCommand "export CLHEP_LIBRARY=/clhep-install/lib"
+executeCommand "LD_LIBRARY_PATH=${CLHEP_LIBRARY}:${LD_LIBRARY_PATH}"
+executeCommand "PATH=/clhep-install/CLHEP/bin:$PATH"
+executeCommand "source /geant4-install/bin/geant4.sh"
+executeCommand "source /root-system/bin/thisroot.sh"
+executeCommand "export LD_LIBRARY_PATH=/CADMesh-install/lib:${LD_LIBRARY_PATH}"
+executeCommand "cmake .."
+executeCommand "make"
+
