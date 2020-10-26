4a857b9ab57afad2cd1168a46f57fc9a6d5e5953
kdulski <30865956+kdulski@users.noreply.github.com>
2020-10-26 10:22:11 +0100

    Update EventAction.cpp

--- Actions/EventAction.cpp
+++ Actions/EventAction.cpp
@@ -86,13 +86,12 @@ void EventAction::WriteToFile(const G4Event* anEvent)
     DHC = dynamic_cast<DetectorHitsCollection*>(HCE->GetHC(fScinCollID));
     int n_hit = DHC->entries();
     for (int i = 0; i < n_hit; i++) {
-      DetectorHit* dh =  dynamic_cast<DetectorHit*>(DHC->GetHit(i));
+      DetectorHit* dh = dynamic_cast<DetectorHit*>(DHC->GetHit(i));
      
       // Forcefully cut the remnants from the cut on photon durinng first interaction----     
-       double EnergyDeposit = dh->GetEdep();
-      
-       // Removing remnants from the energy deposition cut on prim photon
-       if (EnergyDeposit < .511 - fEvtMessenger->GetEnergyCut() && fEvtMessenger->GetEnergyCutFlag()) continue;  
+      double EnergyDeposit = dh->GetEdep();
+      // Removing remnants from the energy deposition cut on prim photon
+      if (EnergyDeposit < .511 - fEvtMessenger->GetEnergyCut() && fEvtMessenger->GetEnergyCutFlag()) continue;  
      
        fHistoManager->AddNewHit(dh);
     }
